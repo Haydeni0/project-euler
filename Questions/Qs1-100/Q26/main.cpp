226df73349e30f3168d956630ae973ed9ebc4d83
Haydeni0 <hayden.dorahy@gmail.com>
2021-02-14 17:44:26 +0000

    Complete Q26

--- Questions/Qs1-100/Q26/main.cpp
+++ Questions/Qs1-100/Q26/main.cpp
@@ -17,11 +17,61 @@ Find the value of d < 1000 for which 1/d contains the longest recurring cycle in
 
 #include "UsefulFunctions.h"
 
+int cycleLength(const VectorFloat VF)
+{ // Returns the cycle length of a recurring number. Doesn't really work for values such as 0.2, thinks its a cycle length of 1.
+    std::vector<short> mantissa{VF.get_mantissa()};
+    for (int start_idx{0}; start_idx < VF.size(); ++start_idx)
+    {
+        for (int end_idx{start_idx}; end_idx < VF.size(); ++end_idx)
+        {
+            std::vector<short> cycle{mantissa.begin() + start_idx, mantissa.begin() + end_idx + 1};
+            int mantissa_idx{0};
+            while (mantissa_idx < VF.size() && mantissa[mantissa_idx] != cycle[0])
+                mantissa_idx++;
+            int cycle_idx{0};
+            bool is_recursive{true};
+            while (mantissa_idx < VF.size() - 1)
+            {
+                if (mantissa[mantissa_idx] == cycle[cycle_idx])
+                {
+                    cycle_idx++;
+                    mantissa_idx++;
+                    if (cycle_idx == cycle.size())
+                        cycle_idx = 0;
+                }
+                else
+                {
+                    is_recursive = false;
+                    break;
+                }
+            }
+            // If it makes it this far, then cycle is a recursive cycle
+            if (is_recursive && (end_idx < (VF.size() - 2)))
+                return cycle.size();
+        }
+    }
+    // If it makes it this far, then there is no recursive cycle
+    return 0;
+}
 
 int main()
 {
-    
+    int num_sigfigs = 1000;
+    VectorFloat::s_div_sigfigs = num_sigfigs;
+    int biggest_cycle{0};
+    for (VectorFloat d{3}; d < 1000; d += 2)
+    {
+        VectorFloat result{1/d};
+        int cycle_length{cycleLength(result)};
 
+        if (cycle_length > biggest_cycle)
+        {
+            biggest_cycle = cycle_length;
+            std::cout << "1/"<< d << " has cycle Length: " << cycle_length << "\n";
+        }
+    }
+
+    std::cout << cycleLength(1 / VectorFloat(75)) << "\n";
 
     return 0;
 }
