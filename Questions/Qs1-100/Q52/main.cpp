7007ee1288e2809e278fb8d8e30e525d60d8fde7
Haydeni0 <hayden.dorahy@gmail.com>
2021-04-17 19:22:55 +0100

    Completed Q52

--- Questions/Qs1-100/Q52/main.cpp
+++ Questions/Qs1-100/Q52/main.cpp
@@ -1,11 +1,32 @@
 /* 
+It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
 
+Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
 */
 
 #include "UsefulFunctions.h"
 
 int main()
 {
-    
+    int n{1};
+
+    while (true)
+    {
+        bool all_true{true};
+        uf::UnorderedDigitID N{n};
+        for(int mult{2}; mult <= 6; mult ++)
+        {
+            uf::UnorderedDigitID N_mult{n*mult};
+            if (!(N == N_mult))
+                all_true = false;
+        }
+        if (all_true)
+        {
+            std::cout << n;
+            break;
+        }
+        ++n;
+    }
+
     return 0;
 }
